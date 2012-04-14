c1fb21922577e0ee7bcc0e63d7cd4ab6c9e63c13
Ben Bullock <benkasminbullock@gmail.com>
2012-04-14 16:48:16 +0900

    Removed pointless things from documentation.

--- lib/Image/PNG/FileConvert.pm
+++ lib/Image/PNG/FileConvert.pm
@@ -241,12 +241,6 @@ L</png2file>.
 
 =back
 
-=head1 WHY?
-
-This module is for people who want to sneakily use free photo/image
-sharing websites, free mail services, or other such dumbasseries to
-store and retrieve data files.
-
 =head1 AUTHOR
 
 Ben Bullock, <bkb@cpan.org>
@@ -255,8 +249,6 @@ Ben Bullock, <bkb@cpan.org>
 
 You can use, modify and distribute this software under the Perl
 Artistic Licence or the GNU General Public Licence.
-
-=head1 DIAGNOSTICS
 
 =head1 SEE ALSO
 
